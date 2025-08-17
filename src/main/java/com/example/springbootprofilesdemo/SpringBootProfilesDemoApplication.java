ee7f850fcdc4a244a2a3567c9781e80d1e47f974
Kleber Vales <klebervales.dev@gmail.com>
2025-08-17 17:10:13 -0300

    built: initialization java with spring boot application

---
+++ src/main/java/com/example/springbootprofilesdemo/SpringBootProfilesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootprofilesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfilesDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProfilesDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootprofilesdemo/SpringBootProfilesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootprofilesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProfilesDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
